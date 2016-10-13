937d9b8522edef2dce05021460c4d9f699f0586f
vmaksym <maksymchuk.v@gmail.com>
2016-10-13 14:52:22 +0300

    Added image loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ module.exports= {
         test: /\.scss$/,
         loader: 'style-loader!css-loader!sass-loader'
 
+      }, {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=60000'
       }
     ]
   }
